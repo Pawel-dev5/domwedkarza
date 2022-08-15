513df417d6a7110fba86f825d6ade5f47a73300c
PawelNowecki <p.nowecki@gridaly.com>
2022-08-15 23:55:08 +0200

    fix font import

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,10 @@ export default class MyDocument extends Document {
 				<Head>
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
-
-					<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&family=Poppins:wght@300;400;600;700&family=Roboto:wght@400;500;700&display=swap" />
+					<link
+						href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&family=Poppins:wght@300;400;600;700&family=Roboto:wght@400;500;700&display=swap"
+						rel="stylesheet"
+					/>
 				</Head>
 
 				<body>
