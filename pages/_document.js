5f6151054d92888a5c71173c47ddfd1a51dbc8ef
PawelNowecki <p.nowecki@gridaly.com>
2022-08-23 16:29:57 +0200

    [F] Add two tags : GTM and GA

--- pages/_document.js
+++ pages/_document.js
@@ -6,18 +6,31 @@ export default class MyDocument extends Document {
 		return (
 			<Html lang="pl">
 				<Head>
-					{/* <!-- Google Tag Manager --> */}
+					{/* <!-- Google Analytics --> */}
 					<script async src="https://www.googletagmanager.com/gtag/js?id=G-93GN096F3Y" />
 					<script
 						dangerouslySetInnerHTML={{
 							__html: `window.dataLayer = window.dataLayer || [];
 							function gtag(){dataLayer.push(arguments);}
 							gtag('js', new Date());
-						  
+							
 							gtag('config', 'G-93GN096F3Y');`,
 						}}
 					/>
-					{/* <!-- End Google Tag Manager --> */}
+					{/* <!-- END Google Analytics --> */}
+
+					{/* <!-- Google Tag Manager --> */}
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `(function(w,d,s,l,i){
+							w[l]=w[l]||[];
+							w[l].push({'gtm.start':	new Date().getTime(),event:'gtm.js'});
+							var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+							j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+							})(window,document,'script','dataLayer','GTM-MNP6WP4');`,
+						}}
+					/>
+					{/* <!-- END Google Tag Manager --> */}
 
 					{/* FONTS */}
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -25,6 +38,13 @@ export default class MyDocument extends Document {
 				</Head>
 
 				<body>
+					{/* <!-- Google Tag Manager (noscript) --> */}
+					<noscript
+						dangerouslySetInnerHTML={{
+							__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNP6WP4" height="0" width="0" style="display:none;visibility:hidden" />`,
+						}}
+					/>
+					{/* <!-- End Google Tag Manager (noscript) --> */}
 					<Main />
 					<NextScript />
 				</body>
